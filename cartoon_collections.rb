4b0dc0f084b122bd5beba4d405095454d3ef683b
Kristen Peck <kpeck71@gmail.com>
2017-09-17 20:45:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,5 @@ end
 
 def find_the_cheese(list)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  list.find{|cheese| cheese = (list & cheese_types)}
+  list.find {|cheese| cheese_types.include?(cheese)}
 end
